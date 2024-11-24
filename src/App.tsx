b0f3529bb72963257a5e94bf41b018ed7ccf3d59
Joel Cutler <joelcutler108@gmail.com>
2024-11-24 15:28:50 +0000

    added page navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
 import React, { useState } from "react";
-import { FlatList, SafeAreaView, Text, TouchableOpacity, View } from "react-native";
 
 import { v4 as uuidv4 } from "uuid";
-import ListItem from "./components/ListItem";
-import MileageBox from "./components/MileageBox";
-import RecordModal from "./components/RecordModal";
-import useRecordModal from "./hooks/useRecordModal";
-import { styles as globalStyles } from "./styles";
-import Record from "./types/Record";
 
-import { Feather } from "@expo/vector-icons";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import "react-native-get-random-values";
-import RecordsNoSelection from "./components/RecordsNoSelection";
 import useCachedRecords from "./hooks/useCachedRecords";
+import AppScreen from "./screens/AppScreen";
+import SettingsScreen from "./screens/SettingsScreen";
 
 // todo settings page miles/km, currency
 // todo settings page export, import
@@ -46,58 +41,47 @@ export default function App() {
 		{ id: uuidv4(), type: "mileage", mileage: 60449 },
 	]);
 
-	const { modalOpen, record: modalRecord, setRecord: setModalRecord, openModal, closeModal } = useRecordModal();
-
 	const [calculateRecordsNo, setCalculateRecordsNo] = useState<RecordNo>("all");
+
+	const Stack = createNativeStackNavigator();
 
 	return (
 		<>
-			<SafeAreaView style={globalStyles.body}>
-				<View style={{ display: "flex", flexDirection: "column", gap: 10, flex: 1 }}>
-					<View style={{ display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
-						{/* Settings Button */}
-						<TouchableOpacity activeOpacity={0.6} style={globalStyles.blankButton}>
-							<Feather name="settings" color={globalStyles.blankButtonText.color} size={18} />
-						</TouchableOpacity>
-
-						{/* Calculated Records No */}
-						<RecordsNoSelection calculateRecordsNo={calculateRecordsNo} setCalculateRecordsNo={setCalculateRecordsNo} />
-					</View>
-
-					{/* Milage Box */}
-					<MileageBox
-						cachedRecordTransitions={cachedRecordTransitions}
-						calculateRecordsNo={calculateRecordsNo}
-						openModal={() =>
-							openModal({ id: null, type: "record", mileage: records.length > 0 ? records[0].mileage : 0, cost: 0 })
-						}
-					/>
-
-					{/* List of records */}
-					<FlatList
-						style={{ borderRadius: 8 }}
-						data={records}
-						renderItem={({ item, index }: { item: Record; index: number }) => (
-							<ListItem
-								item={item}
-								cachedTransition={cachedRecordTransitions[index]}
-								openModal={() => openModal(item)}
+			<NavigationContainer>
+				<Stack.Navigator initialRouteName="App" screenOptions={{ headerShown: false }}>
+					<Stack.Screen
+						name="App"
+						component={(
+							props: any // todo work out type
+						) => (
+							<AppScreen
+								{...props}
+								calculateRecordsNo={calculateRecordsNo}
+								setCalculateRecordsNo={setCalculateRecordsNo}
+								cachedRecordTransitions={cachedRecordTransitions}
+								records={records}
+								updateRecord={updateRecord}
+								removeRecord={removeRecord}
 							/>
 						)}
-						keyExtractor={(item: Record) => item.id!}
-						ListEmptyComponent={<Text style={{ textAlign: "center" }}>No Records Yet</Text>}
 					/>
-				</View>
-			</SafeAreaView>
+					<Stack.Screen
+						name="Settings"
+						component={(
+							props: any // todo work out type
+						) => <SettingsScreen {...props} />}
+					/>
+				</Stack.Navigator>
+			</NavigationContainer>
 
-			<RecordModal
-				modalOpen={modalOpen}
-				record={modalRecord}
-				setRecord={setModalRecord}
-				closeModal={closeModal}
-				updateRecord={updateRecord}
-				removeRecord={removeRecord}
-			/>
+			{/* <Main
+					calculateRecordsNo={calculateRecordsNo}
+					setCalculateRecordsNo={setCalculateRecordsNo}
+					cachedRecordTransitions={cachedRecordTransitions}
+					records={records}
+					updateRecord={updateRecord}
+					removeRecord={removeRecord}
+				/> */}
 		</>
 	);
 }
---
+++ src/screens/AppScreen.tsx
@@ -0,0 +1,88 @@
+import { Feather } from "@expo/vector-icons";
+import React from "react";
+import { FlatList, SafeAreaView, Text, TouchableOpacity, View } from "react-native";
+import { RecordNo } from "../App";
+import ListItem from "../components/ListItem";
+import MileageBox from "../components/MileageBox";
+import RecordModal from "../components/RecordModal";
+import RecordsNoSelection from "../components/RecordsNoSelection";
+import { CachedRecordTransition } from "../hooks/useCachedRecords";
+import useRecordModal from "../hooks/useRecordModal";
+import { styles as globalStyles } from "../styles";
+import Record from "../types/Record";
+import { useStateSetter } from "../types/utils";
+
+export default function AppScreen({
+	navigation,
+	calculateRecordsNo,
+	setCalculateRecordsNo,
+	cachedRecordTransitions,
+	records,
+	updateRecord,
+	removeRecord,
+}: {
+	navigation: any; // todo work out type
+	calculateRecordsNo: RecordNo;
+	setCalculateRecordsNo: useStateSetter<RecordNo>;
+	cachedRecordTransitions: CachedRecordTransition[];
+	records: Record[];
+	updateRecord: (record: Record) => void;
+	removeRecord: (record: Record) => void;
+}) {
+	const { modalOpen, record: modalRecord, setRecord: setModalRecord, openModal, closeModal } = useRecordModal();
+
+	return (
+		<>
+			<SafeAreaView style={globalStyles.body}>
+				<View style={{ display: "flex", flexDirection: "column", gap: 10, flex: 1 }}>
+					<View style={{ display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
+						{/* Settings Button */}
+						<TouchableOpacity
+							activeOpacity={0.6}
+							style={globalStyles.blankButton}
+							onPress={() => navigation.navigate("Settings")}
+						>
+							<Feather name="settings" color={globalStyles.blankButtonText.color} size={18} />
+						</TouchableOpacity>
+
+						{/* Calculated Records No */}
+						<RecordsNoSelection calculateRecordsNo={calculateRecordsNo} setCalculateRecordsNo={setCalculateRecordsNo} />
+					</View>
+
+					{/* Milage Box */}
+					<MileageBox
+						cachedRecordTransitions={cachedRecordTransitions}
+						calculateRecordsNo={calculateRecordsNo}
+						openModal={() =>
+							openModal({ id: null, type: "record", mileage: records.length > 0 ? records[0].mileage : 0, cost: 0 })
+						}
+					/>
+
+					{/* List of records */}
+					<FlatList
+						style={{ borderRadius: 8 }}
+						data={records}
+						renderItem={({ item, index }: { item: Record; index: number }) => (
+							<ListItem
+								item={item}
+								cachedTransition={cachedRecordTransitions[index]}
+								openModal={() => openModal(item)}
+							/>
+						)}
+						keyExtractor={(item: Record) => item.id!}
+						ListEmptyComponent={<Text style={{ textAlign: "center" }}>No Records Yet</Text>}
+					/>
+				</View>
+			</SafeAreaView>
+
+			<RecordModal
+				modalOpen={modalOpen}
+				record={modalRecord}
+				setRecord={setModalRecord}
+				closeModal={closeModal}
+				updateRecord={updateRecord}
+				removeRecord={removeRecord}
+			/>
+		</>
+	);
+}
