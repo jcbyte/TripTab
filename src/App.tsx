344446b7d31241bcf5eefe956b3c23d3b8874c5c
Joel Cutler <joelcutler108@gmail.com>
2024-11-25 17:49:40 +0000

    colour theme got from device

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import React from "react";
+import { useColorScheme } from "react-native";
 import ThemedApp from "./ThemedApp";
 import { ThemeProvider } from "./hooks/Theme";
 import { themes } from "./styles";
 
+// todo theme status bar
 // todo save records + settings
+// todo save theme to settings
+// todo readme
 
 export default function App() {
+	const scheme = useColorScheme();
+
 	return (
-		<ThemeProvider initialTheme={themes.light}>
+		<ThemeProvider initialTheme={scheme === "dark" ? themes.dark : themes.light}>
 			<ThemedApp />
 		</ThemeProvider>
 	);
